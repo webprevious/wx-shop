a89b8035ee5f45f9d895b60f64e6b6c63d67e755
bs <228713680@qq.com>
2019-03-01 18:06:20 +0800

    tabbar

---
+++ src/components/tabbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <cover-view class="tabbar">
+
+  </cover-view>
+</template>
+
+<script>
+export default {
+  data () {},
+}
+</script>
+
+<style>
+.tabbar {
+  height: 100rpx;
+  width: 750rpx;
+  background: red;
+}
+</style>
--- src/pages/tabbar/index/index.vue
+++ src/pages/tabbar/index/index.vue
@@ -3,6 +3,9 @@
     <div class="btn-wrap">
       <van-button type="info" @click="getMsg">发一个请求</van-button>
     </div>
+    <div>
+      <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="getUserInfo">获取用户信息</button>
+    </div>
   </div>
 </template>
 
@@ -16,10 +19,16 @@ export default {
     async getMsg () {
       const res = await this.$request('/test')
       console.log(res)
+    },
+    getUserInfo (res) {
+      console.log(1111)
+      console.log(res)
+    },
+    getPhoneNumber (res) {
+      console.log(res)
     }
   },
   created () {
-    // let app = getApp()
   }
 }
 </script>
