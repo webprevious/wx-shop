7b506c286c04298e031b1f53820ea1f5737d0d89
bs <228713680@qq.com>
2019-03-10 00:52:46 +0800

    优化发布物品时的用户体验，在点击发布那一刻才去上传图片，添加loading

--- src/pages/publish/index.vue
+++ src/pages/publish/index.vue
@@ -4,14 +4,17 @@
       <textarea maxlength="50" v-model="goodsTitle" class="title" placeholder="请输入发布物品的标题"/>
       <textarea maxlength="-1" v-model="goodsDesc" class="goods-detail" placeholder="物品详情描述"/>
       <div class="upload-img-wrap">
-        <div class="add-icon-wrap" @click="imgsUpload">
+        <div v-for="(url,index) in wxLocalImgUrls" class="upload-img" :key="index">
+          <img :src="url" class="img">
+        </div>
+        <div class="add-icon-wrap" @click="selectImgs">
           <img class="icon" src="../../../static/images/upload-icon.png">
         </div>
       </div>
       <input class="price-input" v-model="goodsPrice" type="digit" placeholder="请输入物品价格"/>
-      <picker class="picker-wrap" @change="CategoryPickerChange" :value="index" :range="objectArray" range-key="name">
+      <picker class="picker-wrap" @change="CategoryPickerChange" :value="categoryIndex" :range="objectArray" range-key="name">
         <view class="picker">
-          当前选择：{{objectArray[index].name}}
+          当前选择：{{objectArray[categoryIndex].name}}
         </view>
       </picker>
       <div class="bussiness-wrap">
@@ -27,7 +30,7 @@
         <input class="input" v-model="wechat" type="text" placeholder="请输入微信">
         <input class="input" v-model="phone" type="number" placeholder="请输入手机号">
       </div>
-      <div class="comfirm-publish-btn" @click="comfirmPublish">确认发布</div>
+      <div class="comfirm-publish-btn" @click="imgsUploadAndConfirm">确认发布</div>
     </div>
   </div>
 </template>
@@ -35,44 +38,39 @@
 <script>
 import MyCheckbox from '@/components/mycheckbox.vue'
 import utils from '@/utils/index.js'
+import { mapState } from 'vuex'
 export default {
   data () {
     return {
       goodsTitle: '',
       goodsDesc: '',
       goodsPrice: '',
+      // 交易方式选择结果
       bussPath: [],
       qq: '',
       wechat: '',
       phone: '',
-      list: ['a', 'b', 'c'],
-      result: [],
-      index: 0,
-      objectArray: [
-        {
-          id: 0,
-          name: '美国'
-        },
-        {
-          id: 1,
-          name: '中国'
-        },
-        {
-          id: 2,
-          name: '巴西'
-        },
-        {
-          id: 3,
-          name: '日本'
-        }
-      ],
-      upyun: {}
+      // 物品分类选择结果
+      categoryIndex: 0,
+      // 物品分类选择器数组
+      objectArray: [{
+        id: '0',
+        name: '未选择'
+      }],
+      upyun: {},
+      // 上传成功的图片url地址
+      uploadSuccessImgsUrl: [],
+      // 微信本地url用来展示提示用户体验，在用户确定发布的时候才去真实上传图片
+      wxLocalImgUrls: []
     }
   },
   computed: {
     isInputConnect () {
       return this.qq || this.wechat || this.phone
-    }
+    },
+    ...mapState({
+      userInfo: state => state.userInfo.userInfo
+    })
   },
   watch: {
     phone (newValue) {
@@ -90,38 +88,34 @@ export default {
   methods: {
     // 分类picker回调
     CategoryPickerChange (e) {
-      console.log(e)
-      this.index = e.target.value
+      this.categoryIndex = e.target.value
     },
     // 交易方式回调
     bussSelectChange (value) {
-      console.log(value)
+      // console.log(value)
       this.bussPath = value
-    },
-    // 确认按钮
-    comfirmPublish () {
-      let inputCheck = this.inputCheck()
-      if (inputCheck) {
-        return this.$toast(inputCheck)
-      }
-      let data = this.createReqData()
-      console.log(data)
     },
     // 收集确认发布数据
     createReqData () {
       let data = {
+        publisherId: this.userInfo._id,
         goodsTitle: this.goodsTitle,
-        goodsDesc: this.goodsDesc,
-        goodsPrice: this.goodsPrice
+        goodsDetailMsg: this.goodsDesc,
+        goodsPrice: this.goodsPrice,
+        goodsCategoryId: this.objectArray[this.categoryIndex].id,
+        goodsBussPath: this.bussPath
+      }
+      if (this.uploadSuccessImgsUrl.length) {
+        data.goodsPics = this.uploadSuccessImgsUrl
       }
       if (this.qq) {
-        data.qq = this.qq
+        data.goodsConnQq = this.qq
       }
       if (this.wechat) {
-        data.wechat = this.wechat
+        data.goodsConnWx = this.wechat
       }
       if (this.phone) {
-        data.phone = this.phone
+        data.goodsConnPhone = this.phone
       }
       return data
     },
@@ -134,6 +128,8 @@ export default {
         err = '请输入物品描述'
       } else if (!this.goodsPrice) {
         err = '请输入物品价格'
+      } else if (this.categoryIndex === 0) {
+        err = '请选择物品分类'
       } else if (!this.bussPath.length) {
         err = '请选择交易方式'
       } else if (!this.isInputConnect) {
@@ -142,19 +138,14 @@ export default {
       return err
     },
     // 上传图片
-    imgsUpload () {
+    selectImgs () {
       let that = this
       wx.chooseImage({
         success (selectRes) {
-          console.log(selectRes)
-          let promiseArr = []
+          // console.log(selectRes)
           selectRes.tempFilePaths.forEach(item => {
-            promiseArr.push(utils.uploadFile(item, that.upyun))
-          })
-          Promise.all(promiseArr).then(res => {
-            console.log(res)
-          }).catch(err => {
-            console.log(err)
+            // promiseArr.push(utils.uploadFile(item, that.upyun))
+            that.wxLocalImgUrls.push(item)
           })
         },
         fail (err) {
@@ -162,17 +153,77 @@ export default {
         }
       })
     },
+    // 点击确认按钮后先检查字段然后真实上传图片，然后发送请求物品信息
+    imgsUploadAndConfirm () {
+      // 检查字段
+      let inputCheck = this.inputCheck()
+      if (inputCheck) {
+        return this.$toast(inputCheck)
+      }
+      // 检查字段无误开启loading
+      wx.showLoading({
+        title: '正在发布'
+      })
+      // 上传图片，成功后发送请求信息
+      let promiseArr = []
+      this.wxLocalImgUrls.forEach(item => {
+        promiseArr.push(utils.uploadFile(item, this.upyun))
+      })
+      Promise.all(promiseArr).then(async res => {
+        // console.log(res)
+        res.forEach(item => {
+          // console.log(JSON.parse(item.data))
+          let url = 'http://shopdev.test.upcdn.net/' + JSON.parse(item.data).url
+          this.uploadSuccessImgsUrl.push(url)
+        })
+        // 请求后端接口发送物品信息
+        let data = this.createReqData()
+        console.log(data)
+        const result = await this.$request('/publishGoods', data, 'POST')
+        if (result.code) {
+          this.$toast('发布成功', 'success')
+          setTimeout(function () {
+            wx.navigateTo({
+              url: '/pages/home/main'
+            })
+          }, 1500)
+        } else {
+          this.$toast('网络错误')
+        }
+      }).catch(err => {
+        this.$toast('发布失败，请稍后重试')
+        console.log(err)
+      })
+    },
     // 获取upyun签名
     async getUpyun () {
       const res = await this.$request('/getUpyun')
       if (res.code) {
-        console.log(res.data)
+        // console.log(res.data)
         this.upyun = res.data
+      } else {
+        this.$toast('网络错误')
+      }
+    },
+    // 获取物品分类
+    async getGoodsCategory () {
+      const res = await this.$request('/getGoodsCateory')
+      if (res.code) {
+        res.data.forEach(item => {
+          let obj = {
+            id: item._id,
+            name: item.title
+          }
+          this.objectArray.push(obj)
+        })
+      } else {
+        this.$toast('网络错误')
       }
     }
   },
   mounted () {
     this.getUpyun()
+    this.getGoodsCategory()
   }
 }
 </script>
@@ -226,6 +277,17 @@ export default {
         .icon {
           width: 48rpx;
           height: 48rpx;
+        }
+      }
+      .upload-img 
+      {
+        width: 210rpx;
+        height: 210rpx;
+        border-radius: 12rpx;
+        .img {
+          width: 210rpx;
+          height: 210rpx;
+          border-radius: 12rpx;
         }
       }
     }
