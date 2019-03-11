71d2580eca1cb2d0adaa8165bfea525619642293
bs <228713680@qq.com>
2019-03-11 23:57:59 +0800

    我出售的页面接口接入

--- src/pages/mysale/index.vue
+++ src/pages/mysale/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="my-buy-wrap">
-    <div class="my-buy-item">
-      <img class="goods-img" src="https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJZjQNfwkJNblWMzsLpW6W2N5xU5QUVFpRvu9zVRQSC67TRTDTBfoIibN1vaoxRBHy8AKZHWic7jdXA/132"/>
+    <div class="my-buy-item" v-for="(item, index) in mySaleArray" :key="index">
+      <img class="goods-img" :src="item.goodsFirstPic"/>
       <div class="goods-msg">
-        <div class="goods-title">商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题</div>
+        <div class="goods-title">{{item.goodsTitle}}</div>
         <div class="buy-time-price">
-          <div class="time">出售于2019-03-06</div>
-          <price price="999.9"></price>
+          <div class="time">出售于{{item.publishAt}}</div>
+          <price :price="String(item.goodsPrice)"></price>
         </div>
       </div>
     </div>
@@ -15,18 +15,37 @@
 
 <script>
 import Price from '@/components/price.vue'
+import { mapState } from 'vuex'
 export default {
   data () {
     return {
+      mySaleArray: []
     }
+  },
+  computed: {
+    ...mapState({
+      userInfo: state => state.userInfo.userInfo
+    })
   },
   components: {
     Price
   },
   methods: {
-    getMsg () {
-      console.log('我的页面')
+    // 获取我卖出的物品列表
+    async getMySale () {
+      const res = await this.$request('/getMyPublishOrSale', {publisherId: this.userInfo._id, goodsStatus: 'be_sale'}, 'POST')
+      if (res.code) {
+        this.mySaleArray = res.data.map(item => {
+          item.publishAt = item.publishAt.slice(0, 10)
+          return item
+        })
+      } else {
+        this.mySaleArray = []
+      }
     }
+  },
+  mounted () {
+    this.getMySale()
   }
 }
 </script>
