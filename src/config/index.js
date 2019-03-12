08c603e3263195e1ac2eb882a54ea92362920d25
bs <228713680@qq.com>
2019-03-12 14:25:18 +0800

    手机ip测试

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,8 @@
 // 请求基础地址
 // export const BASE_URL = 'http://localhost:3000'
+
 // 手机测试需要连接ip
-export const BASE_URL = 'http://192.168.1.109:3000'
+// export const BASE_URL = 'http://192.168.1.109:3000'
+
+// 手机测试需要连接ip
+export const BASE_URL = 'http://10.20.70.114:3000'
