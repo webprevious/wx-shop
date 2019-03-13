21a8b655fbd154533a38f01531007988634b4032
bs <228713680@qq.com>
2019-03-13 11:58:09 +0800

    我的收藏接口接入，点击可以跳转至详情

--- src/pages/mycollection/index.vue
+++ src/pages/mycollection/index.vue
@@ -1,16 +1,6 @@
 <template>
   <div class="my-buy-wrap">
-    <!-- <div class="my-buy-item">
-      <img class="goods-img" src="https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJZjQNfwkJNblWMzsLpW6W2N5xU5QUVFpRvu9zVRQSC67TRTDTBfoIibN1vaoxRBHy8AKZHWic7jdXA/132"/>
-      <div class="goods-msg">
-        <div class="goods-title">商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题</div>
-        <div class="buy-time-price">
-          <div class="time">收藏于2019-03-06</div>
-          <price price="999.9"></price>
-        </div>
-      </div>
-    </div> -->
-    <div class="my-buy-item" v-for="(item, index) in myStoreArray" :key="index">
+    <div class="my-buy-item" v-for="(item, index) in myStoreArray" :key="index" @click="goToDetail(item)">
       <img class="goods-img" :src="item.goodsFirstPic"/>
       <div class="goods-msg">
         <div class="goods-title">{{item.goodsTitle}}</div>
@@ -26,11 +16,17 @@
 
 <script>
 import Price from '@/components/price.vue'
+import { mapState, mapActions } from 'vuex'
 export default {
   data () {
     return {
       myStoreArray: []
     }
+  },
+  computed: {
+    ...mapState({
+      userInfo: state => state.userInfo.userInfo
+    })
   },
   components: {
     Price
@@ -40,10 +36,22 @@ export default {
       const res = await this.$request('/storeList', { storeMan: this.userInfo._id }, 'POST')
       if (res.code) {
         console.log(res)
+        this.myStoreArray = res.data.map(item => {
+          item.goodsId.publishAt = item.goodsId.publishAt.slice(0, 10)
+          return item.goodsId
+        })
       } else {
-        console.log(res)
+        this.myStoreArray = []
       }
-    }
+    },
+    // 点击前往详情
+    goToDetail (item) {
+      this.saveCurrentGoodsMessage(item)
+      wx.navigateTo({
+        url: '/pages/goodsdetail/main?from=mystore'
+      })
+    },
+    ...mapActions(['saveCurrentGoodsMessage'])
   },
   mounted () {
     this.getMyStore()
